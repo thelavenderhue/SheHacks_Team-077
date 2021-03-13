bfdecf3f735a169606b113f704ce84feabb7cbbb
Mrinal Bhave <mrinalbhave27@gmail.com>
2021-03-14 01:51:19 +0530

    contact and signup page

--- app.js
+++ app.js
@@ -5,26 +5,27 @@ const express = require("express"),
   store = require("./middleware/multer"),
   UploadModel = require("./model/schema"),
   fs = require("fs"),
-{ allowedNodeEnvironmentFlags } = require("process"),
-  flash=require("connect-flash"),
-  passport=require("passport"),
-  LocalStrategy=require("passport-local"),
+  { allowedNodeEnvironmentFlags } = require("process"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
   { session } = require("passport"),
-  User=require("./model/user");
+  User = require("./model/user");
 
-
-const userRoutes = require('./routes/users');
+const userRoutes = require("./routes/users");
 const app = express();
 require("dotenv").config();
 
 //app.use(session(sessionConfig));
 
 //PASSPORT CONFIGURATION
-app.use(require("express-session")({
-	secret: "Nothing is Greater than LOVE!!!",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    secret: "Nothing is Greater than LOVE!!!",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(flash());
@@ -35,11 +36,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 //currentUseradded through each route to show login, logout and signup
-app.use(function(req,res,next){
-	res.locals.currentUser=req.user;
-	res.locals.error= req.flash("error");
-	res.locals.success= req.flash("success");
-	next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 mongoose.set("useUnifiedTopology", true);
@@ -57,13 +58,13 @@ mongoose.connect(
   }
 );
 
-require('./server/database/database')();
+require("./server/database/database")();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use("/public/images/", express.static("./public/images"));
 
-app.use("/",userRoutes);
+app.use("/", userRoutes);
 
 // app.get("/", (req, res) => {
 //   res.render("home");
@@ -136,17 +137,15 @@ app.get("/joinUs", (req, res) => {
   res.render("joinUs");
 });
 
-app.get("/help", async(req, res) => {
-
+app.get("/help", async (req, res) => {
   const all_images = await UploadModel.find();
-  res.render("help", { images: all_images});
-  
+  res.render("help", { images: all_images });
 });
 
 app.post("/uploadmultiple", store.array("images", 12), (req, res, next) => {
   const files = req.files;
 
-  if(!files) {
+  if (!files) {
     const error = new Error("Please choose files");
     error.httpStatusCode = 400;
     return next(error);
@@ -155,45 +154,53 @@ app.post("/uploadmultiple", store.array("images", 12), (req, res, next) => {
   //convert images into base64 encoding
   let imgArray = files.map((file) => {
     let img = fs.readFileSync(file.path);
-    return encode_image = img.toString('base64');
-  })
+    return (encode_image = img.toString("base64"));
+  });
 
   let result = imgArray.map((src, index) => {
     //create object to store data in the collection
     let finalImg = {
       filename: files[index].originalname,
       contentType: files[index].mimetype,
-      imageBase64: src
-    }
+      imageBase64: src,
+    };
     // console.log(finalImg.filename);
     let newUpload = new UploadModel(finalImg);
     // console.log(newUpload);
     return newUpload
-          .save()
-          .then(() => {
-            return { msg: `${files[index].originalname} image uploaded successfully...!` }
-          })
-          .catch(error => {
-            if(error) {
-              if(error.name === 'MongoError' && error.code === 11000) {
-                return Promise.reject({ error: `Duplicate ${files[index].originalname}.File Already exists!`})
-              }
-              return Promise.reject({error:error.message || `Cannot Upload ${files[index].originalname} Something Missing!`});
-            }
-          })
+      .save()
+      .then(() => {
+        return {
+          msg: `${files[index].originalname} image uploaded successfully...!`,
+        };
+      })
+      .catch((error) => {
+        if (error) {
+          if (error.name === "MongoError" && error.code === 11000) {
+            return Promise.reject({
+              error: `Duplicate ${files[index].originalname}.File Already exists!`,
+            });
+          }
+          return Promise.reject({
+            error:
+              error.message ||
+              `Cannot Upload ${files[index].originalname} Something Missing!`,
+          });
+        }
+      });
   });
 
   // res.json(imgArray);
   Promise.all(result)
-    .then( msg => {
+    .then((msg) => {
       console.log("hey there!");
       // res.json(msg);
       res.redirect("/help");
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
-    })
-})
+    });
+});
 
 app.get("/donate", (req, res) => {
   res.render("donate");
@@ -207,8 +214,8 @@ app.get("/signup", (req, res) => {
   res.render("signUp");
 });
 
-app.get("/contacts", (req, res) => {
-  res.render("contacts");
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 let port = process.env.PORT || 3000;
